cac978f82461779ea87f60b146947cc18ae1d393
Ariakash01 <ariakash067@gmail.com>
2023-09-22 15:07:07 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          second time edited
         </a>
       </header>
     </div>
